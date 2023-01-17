6083a6a50950a97cdd310c79b8d82457f3b4e71c
Morgan Frotten <mmfrotten@gmail.com>
2023-01-17 12:54:24 -0500

    wot

---
+++ data/migrations/20230117155348_first-migration.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async function(knex) {
+  await knex.schema.createTable('projects', table => {
+    table.increments('project_id')
+    table.string('project_name').notNullable()
+    table.string('project_description')
+    table.boolean('project_completed')
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = async function(knex) {
+  await knex.schema.dropTableIfExists('projects')
+};

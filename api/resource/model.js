33db1e739932dded4cf69e70dfb5fcfcce54214d
Morgan Frotten <mmfrotten@gmail.com>
2023-01-19 16:50:55 -0500

    rdkjf

--- api/resource/model.js
+++ api/resource/model.js
@@ -3,8 +3,8 @@ const db = require('../../data/dbConfig')
 
 function add(resource) {
     return db('resources').insert(resource)
-    .then(([id]) => {
-        return db('resources').where('resource_id', id)
+    .then((id) => {
+        return db('resource_name').where('resource_id', id)
     })
 }
 
